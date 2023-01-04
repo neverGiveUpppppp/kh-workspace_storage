35086281803c4eeaa861a3e2b6655d99d6852a44
neverGiveUppppp <csy6219@naver.com>
2023-01-04 18:32:55 +0900

    arranging

--- workspace/3_JDBC_workspace/testJDBC1/src/com/kh/run/Run1.java
+++ workspace/3_JDBC_workspace/testJDBC1/src/com/kh/run/Run1.java
@@ -1,11 +1,11 @@
 package com.kh.run;
+
 import com.kh.view.Menu;
 
-public class Run3 {
-
+public class Run1 {
 	public static void main(String[] args) {
+		
 		new Menu().mainMenu();
-
 	}
 
 }
--- workspace/3_JDBC_workspace/testJDBC2/src/com/kh/run/Run2.java
+++ workspace/3_JDBC_workspace/testJDBC2/src/com/kh/run/Run2.java
@@ -2,7 +2,7 @@ package com.kh.run;
 
 import com.kh.view.MemberMenu;
 
-public class Run {
+public class Run2 {
 
 	public static void main(String[] args) {
 		new MemberMenu().mainMenu();
